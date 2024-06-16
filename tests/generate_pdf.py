47818b917d3a58fd49c77692140e3af3ede974da
csmizzle <chrissmith700@gmail.com>
2024-06-16 15:50:03 -0400

    adding line wrapping

---
+++ tests/generate_pdf.py
@@ -0,0 +1,9 @@
+from constants import REPORT_JSON, BASEDIR
+from conductor.reports.outputs import report_to_pdf
+from conductor.reports.models import Report
+import os
+
+
+if __name__ == "__main__":
+    report = Report(**REPORT_JSON)
+    report_to_pdf(report, os.path.join(BASEDIR, "data", "test_report.pdf"))
