bd8671a7a7c7fa3eed676d0a26a8170d8e36d79e
csmizzle <chrissmith700@gmail.com>
2024-07-11 01:33:38 -0400

    cleaning up, more deletes

--- conductor/utils.py
+++
@@ -1,9 +0,0 @@
-"""
-Utility functions for Conductor
-"""
-
-
-def clean_string(string: str) -> str:
-    string = string.replace("\n", "")
-    string = string.replace("\\n", "")
-    return string
--- tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-from conductor.utils import clean_string
-from tests.constants import TEST_NEWLINE_HTML_DATA
-
-
-def test_clean_string():
-    cleaned_data = clean_string(TEST_NEWLINE_HTML_DATA)
-    assert "\n" not in cleaned_data
