81b159df8318cfe4c0614d695705342d20f82efc
csmizzle <chrissmith700@gmail.com>
2024-08-09 13:32:08 -0400

    adding adaptive retries

--- conductor/llms.py
+++ conductor/llms.py
@@ -5,9 +5,17 @@ from langchain_openai.llms import OpenAI
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_community.llms.bedrock import Bedrock
 from langchain_community.chat_models.bedrock import BedrockChat
+from botocore.config import Config
 import boto3
 
-bedrock_runtime = boto3.client("bedrock-runtime")
+
+bedrock_config = Config(
+    retries={
+        "max_attempts": 10,
+        "mode": "adaptive",
+    },
+)
+bedrock_runtime = boto3.client("bedrock-runtime", config=bedrock_config)
 openai_gpt_4 = OpenAI(temperature=0, max_tokens=2048)
 claude_v2_1 = Bedrock(client=bedrock_runtime, model_id="anthropic.claude-v2:1")
 claude_sonnet = BedrockChat(
