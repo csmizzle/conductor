771e198e08f87600c1abf7b9011682bbc2f0f22c
csmizzle <chrissmith700@gmail.com>
2024-09-01 20:52:19 -0400

    updating search prompt

--- conductor/search/__init__.py
+++ conductor/search/__init__.py
@@ -35,22 +35,23 @@ vector_retriever = ElasticsearchRetriever.from_es_params(
 prompt = ChatPromptTemplate.from_template(
     """Answer the question based only on the context provided.
 
-Include all sources in the answer.
-Each sentence should have at least one source.
+Each sentence should have at least one source. If there are multiple sources for a sentence, separate them like this: [1][2].
+Sources should be in the form of urls.
+If there is not a url and instead a description of an image, use the description in the source.
 Only include the sources that are relevant to the question.
-Never make up information.
 Do not start the answer with "According to the sources" or similar, make the answer sound natural and analytical.
-Use the sources below to add citations to your answer in for form of footnotes.
 
 Example Answer with Sources:
 
-Acme Corp is run by John Doe.[1, 2] He is the CEO of the company.[1] John has a background in finance and has worked in the industry for over 20 years.[2, 3]
+Acme Corp is run by John Doe.[1][2] He is the CEO of the company.[1] John has a background in finance and has worked in the industry for over 20 years.[2][3]
 Sources:
-1. https://example.com/source1
-2. https://example.com/source2
-3. https://example.com/source3
+[1] https://example.com/source1
+[2] https://example.com/source2
+[3] https://example.com/source3
 
 End of Example Answer
+
+Use the sources below to add citations to your answer in for form of footnotes.
 
 Context: {context}
 
