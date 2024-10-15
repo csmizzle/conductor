760cfc01f576cca681f8bfeeffd34dc8fb571517
csmizzle <chrissmith700@gmail.com>
2024-10-15 17:24:53 -0400

    briefings and transformation pipeline done

--- conductor/audio/transform.py
+++ conductor/audio/transform.py
@@ -1,6 +1,7 @@
 """
 Audio brief for a body of text using ElevenLabs
 """
+from conductor.reports.models import ReportV2
 from elevenlabs import VoiceSettings
 from elevenlabs.client import ElevenLabs
 from pydantic import BaseModel
@@ -44,3 +45,143 @@ def text_to_speech_file(
         file_path=output_path,
         response=response,
     )
+
+
+class ReportAudioTransformer:
+    """
+    Generate an audio briefing from a Evrim report
+    """
+
+    def __init__(
+        self,
+        report: ReportV2,
+        api_key: str,
+        voice_id: str,
+        model_id: str,
+        output_format: str = "mp3_22050_32",
+    ) -> None:
+        self.report = report
+        self.api_key = api_key
+        self.voice_id = voice_id
+        self.model_id = model_id
+        self.output_format = output_format
+
+    def transform_paragraph(
+        self,
+        section: int,
+        paragraph: int,
+        output_path: str,
+        stability: float = 0.0,
+        similarity_boost: float = 1.0,
+        style: float = 0.0,
+        use_speaker_boost: bool = True,
+    ) -> SpeechFileResponse:
+        """Generate an audio speech file for a paragraph
+
+        Args:
+            section (int): section index
+            paragraph (int): paragraph index
+            output_path (str): output file path
+            stability (float, optional): stability. Defaults to 0.0.
+            similarity_boost (float, optional): similarity boost. Defaults to 1.0.
+            style (float, optional): style. Defaults to 0.0.
+            use_speaker_boost (bool, optional): use speaker boost. Defaults to True.
+
+        Returns:
+            SpeechFileResponse: file with response
+        """
+        paragraph = self.report.report.sections[section].paragraphs[paragraph]
+        return text_to_speech_file(
+            api_key=self.api_key,
+            text=". ".join(paragraph.sentences),
+            voice_id=self.voice_id,
+            model_id=self.model_id,
+            output_path=output_path,
+            voice_settings=VoiceSettings(
+                stability=stability,
+                similarity_boost=similarity_boost,
+                style=style,
+                use_speaker_boost=use_speaker_boost,
+            ),
+            output_format=self.output_format,
+        )
+
+    def transform_section(
+        self,
+        section: int,
+        output_path: str,
+        stability: float = 0.0,
+        similarity_boost: float = 1.0,
+        style: float = 0.0,
+        use_speaker_boost: bool = True,
+    ) -> SpeechFileResponse:
+        """Generate an audio speech file for a section
+
+        Args:
+            section (int): section index
+            output_path (str): output file path
+            stability (float, optional): stability of voice. Defaults to 0.0.
+            similarity_boost (float, optional): deviate from original voice. Defaults to 1.0.
+            style (float, optional): style drift. Defaults to 0.0.
+            use_speaker_boost (bool, optional): use Elevenlabs speaker boost. Defaults to True.
+
+        Returns:
+            SpeechFileResponse: _description_
+        """
+        text = ""
+        section = self.report.report.sections[section]
+        for paragraph in section.paragraphs:
+            text += ". ".join(paragraph.sentences)
+        return text_to_speech_file(
+            api_key=self.api_key,
+            text=text,
+            voice_id=self.voice_id,
+            model_id=self.model_id,
+            output_path=output_path,
+            voice_settings=VoiceSettings(
+                stability=stability,
+                similarity_boost=similarity_boost,
+                style=style,
+                use_speaker_boost=use_speaker_boost,
+            ),
+            output_format=self.output_format,
+        )
+
+    def transform_report(
+        self,
+        output_path: str,
+        stability: float = 0.0,
+        similarity_boost: float = 1.0,
+        style: float = 0.0,
+        use_speaker_boost: bool = True,
+    ) -> SpeechFileResponse:
+        """Generate an audio speech file for the entire report
+
+        Args:
+            output_path (str): output file path
+            stability (float, optional): stability of voice. Defaults to 0.0.
+            similarity_boost (float, optional): deviate from original voice. Defaults to 1.0.
+            style (float, optional): style drift. Defaults to 0.0.
+            use_speaker_boost (bool, optional): use Elevenlabs speaker boost. Defaults to True.
+
+        Returns:
+            SpeechFileResponse: _description_
+        """
+        text = ""
+        for section in self.report.report.sections:
+            for paragraph in section.paragraphs:
+                text += ". ".join(paragraph.sentences)
+        return text_to_speech_file(
+            api_key=self.api_key,
+            text=text,
+            voice_id=self.voice_id,
+            model_id=self.model_id,
+            output_path=output_path,
+            voice_settings=VoiceSettings(
+                stability=stability,
+                similarity_boost=similarity_boost,
+                style=style,
+                use_speaker_boost=use_speaker_boost,
+            ),
+            output_format=self.output_format,
+        )
--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,6 +1,14 @@
-from conductor.audio.transform import text_to_speech_file, SpeechFileResponse
+from conductor.audio.transform import (
+    text_to_speech_file,
+    SpeechFileResponse,
+    ReportAudioTransformer,
+)
+from conductor.reports.models import ReportV2
+from tests.constants import REPORT_V2_JSON
 from elevenlabs import VoiceSettings
 import os
+
+test_report = ReportV2.model_validate(REPORT_V2_JSON)
 
 
 def test_text_to_speech_file() -> None:
@@ -20,3 +28,51 @@ def test_text_to_speech_file() -> None:
     assert isinstance(speech_response, SpeechFileResponse)
     assert os.path.exists("test.mp3")
     os.remove("test.mp3")
+
+
+def test_paragraph_to_speech_file() -> None:
+    transformer = ReportAudioTransformer(
+        report=test_report,
+        api_key=os.getenv("ELEVENLABS_API_KEY"),
+        voice_id=os.getenv("ELEVENLABS_VOICE_ID"),
+        model_id=os.getenv("ELEVENLABS_MODEL_ID"),
+    )
+    paragraph_transformed = transformer.transform_paragraph(
+        section=0,
+        paragraph=0,
+        output_path="test_paragraph.mp3",
+    )
+    assert isinstance(paragraph_transformed, SpeechFileResponse)
+    assert os.path.exists("test_paragraph.mp3")
+    os.remove("test_paragraph.mp3")
+
+
+def test_section_to_speech_file() -> None:
+    transformer = ReportAudioTransformer(
+        report=test_report,
+        api_key=os.getenv("ELEVENLABS_API_KEY"),
+        voice_id=os.getenv("ELEVENLABS_VOICE_ID"),
+        model_id=os.getenv("ELEVENLABS_MODEL_ID"),
+    )
+    paragraph_transformed = transformer.transform_section(
+        section=0,
+        output_path="test_section.mp3",
+    )
+    assert isinstance(paragraph_transformed, SpeechFileResponse)
+    assert os.path.exists("test_section.mp3")
+    os.remove("test_section.mp3")
+
+
+def test_report_to_speech_file() -> None:
+    transformer = ReportAudioTransformer(
+        report=test_report,
+        api_key=os.getenv("ELEVENLABS_API_KEY"),
+        voice_id=os.getenv("ELEVENLABS_VOICE_ID"),
+        model_id=os.getenv("ELEVENLABS_MODEL_ID"),
+    )
+    paragraph_transformed = transformer.transform_report(
+        output_path="test_report.mp3",
+    )
+    assert isinstance(paragraph_transformed, SpeechFileResponse)
+    assert os.path.exists("test_report.mp3")
+    # os.remove("test_report.mp3")
