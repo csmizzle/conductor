28fd2d528f5c655d597f2b0a9cf1bbe7b48f652d
csmizzle <chrissmith700@gmail.com>
2024-10-24 11:55:18 -0400

    adding report title to agent builder

--- conductor/builder/agent.py
+++ conductor/builder/agent.py
@@ -1,6 +1,8 @@
 import dspy
 from pydantic import BaseModel
 import concurrent.futures
+from functools import partial
+from tqdm import tqdm
 
 claude = dspy.LM("bedrock/anthropic.claude-3-sonnet-20240229-v1:0")
 dspy.configure(lm=claude)
@@ -11,40 +13,54 @@ class ResearchAgentTemplate(BaseModel):
     research_questions: list[str]
 
 
+class ResearchTeamTemplate(BaseModel):
+    title: str
+    agents: list[ResearchAgentTemplate]
+
+
 class AgentBuilderFromReportSection:
-    def __init__(self, section_title: str) -> None:
+    def __init__(self, team_title: str, section_title: str) -> None:
+        self.team_title = team_title
         self.section_title = section_title
 
     def build(self) -> ResearchAgentTemplate:
         agent_title = dspy.ChainOfThought(
-            "company_report_section_title:str -> agent_title:str"
+            "research_team_title:str, report_section_title:str -> agent_title:str"
         )
         generated_title = agent_title(
-            company_report_section_title=self.section_title
+            research_team_title=self.team_title, report_section_title=self.section_title
         ).agent_title
-        research_questions = dspy.ChainOfThought(
-            "agent_title:str -> company_research_questions:list[str]"
+        generated_research_questions = dspy.ChainOfThought(
+            "research_team_title:str, agent_title:str -> research_questions:list[str]"
         )
         return ResearchAgentTemplate(
             title=generated_title,
-            research_questions=research_questions(
-                agent_title=generated_title
-            ).company_research_questions,
+            research_questions=generated_research_questions(
+                research_team_title=self.team_title, agent_title=generated_title
+            ).research_questions,
         )
 
 
-def build_from_section(section_title: str) -> ResearchAgentTemplate:
-    return AgentBuilderFromReportSection(section_title).build()
+def build_from_section(team_title: str, section_title: str) -> ResearchAgentTemplate:
+    return AgentBuilderFromReportSection(
+        team_title=team_title, section_title=section_title
+    ).build()
 
 
 def build_from_report_sections(
+    report_title: str,
     section_titles: list[str],
-) -> list[ResearchAgentTemplate]:
-    return [build_from_section(title) for title in section_titles]
+) -> ResearchTeamTemplate:
+    agents = [build_from_section(report_title, title) for title in tqdm(section_titles)]
+    return ResearchTeamTemplate(title=report_title, agents=agents)
 
 
 def build_from_report_sections_parallel(
+    report_title: str,
     section_titles: list[str],
-) -> list[ResearchAgentTemplate]:
+) -> ResearchTeamTemplate:
+    # build partial function with report title
+    build_from_section_partial = partial(build_from_section, report_title)
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        return list(executor.map(build_from_section, section_titles))
+        agents = list(executor.map(build_from_section_partial, section_titles))
+        return ResearchTeamTemplate(title=report_title, agents=agents)
--- tests/test_agent_builder.py
+++ tests/test_agent_builder.py
@@ -1,7 +1,45 @@
-from conductor.builder.agent import build_from_section, ResearchAgentTemplate
+from conductor.builder.agent import (
+    build_from_section,
+    build_from_report_sections,
+    build_from_report_sections_parallel,
+    ResearchAgentTemplate,
+    ResearchTeamTemplate,
+)
 
 
 def test_build_from_section() -> None:
-    report_title = "Marketing"
-    agent = build_from_section(report_title)
+    report_title = "Person Due Diligence"
+    agent = build_from_section(
+        team_title=report_title, section_title="Person Background"
+    )
     assert isinstance(agent, ResearchAgentTemplate)
+
+
+def test_build_from_report_sections() -> None:
+    report_title = "Person Due Diligence"
+    section_titles = [
+        "Person Background",
+        "Person Employment History",
+        "Person Financial History",
+    ]
+    team = build_from_report_sections(
+        report_title=report_title, section_titles=section_titles
+    )
+    assert isinstance(team, ResearchTeamTemplate)
+    for agent in team.agents:
+        assert isinstance(agent, ResearchAgentTemplate)
+
+
+def test_build_from_report_sections_parallel() -> None:
+    report_title = "Person Due Diligence"
+    section_titles = [
+        "Person Background",
+        "Person Employment History",
+        "Person Financial History",
+    ]
+    team = build_from_report_sections_parallel(
+        report_title=report_title, section_titles=section_titles
+    )
+    assert isinstance(team, ResearchTeamTemplate)
+    for agent in team.agents:
+        assert isinstance(agent, ResearchAgentTemplate)
