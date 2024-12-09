10f9d1fcbab7e8d2cf00831b691eada7bb2809d2
csmizzle <chrissmith700@gmail.com>
2024-12-09 13:49:28 -0500

    think about this workflow more and also consider placing in server

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,20 +1,12 @@
 from conductor.agents.graph import workflow
-import pprint
 
 
 def test_workflow():
     inputs = {
         "messages": [
-            ("user", "What is Steve Rubl"),
+            ("user", "What is Steve Rubley's background?"),
         ]
     }
     graph = workflow.compile()
-    for output in graph.stream(inputs):
-        for key, value in output.items():
-            pprint.pprint(f"Output from node '{key}':")
-            pprint.pprint("---")
-            pprint.pprint(value, indent=2, width=80, depth=None)
-        pprint.pprint("\n---\n")
-
-
-test_workflow()
+    output = graph.invoke(inputs)
+    assert isinstance(output, dict)
